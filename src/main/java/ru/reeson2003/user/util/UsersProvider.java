80cf860b562e6aaf290f1a2db0c376df4896bc8b
P.Gavrilov <p.gavrilov@spb.ant-inform.ru>
2017-09-14 14:26:49 +0300

    Caching provider

--- src/main/java/ru/reeson2003/user/util/UsersProvider.java
+++ src/main/java/ru/reeson2003/user/util/UsersProvider.java
@@ -2,6 +2,7 @@ package ru.reeson2003.user.util;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.reeson2003.user.api.Users;
+import ru.reeson2003.user.caching.CachedUsersImpl;
 
 /**
  * Date: 03.09.2017.
@@ -11,7 +12,12 @@ import ru.reeson2003.user.api.Users;
  */
 public class UsersProvider {
     private static Users users = new ClassPathXmlApplicationContext("user-persist-context.xml").getBean(Users.class);
+
     public static Users getUsers() {
         return users;
+    }
+
+    public static Users getCachingUsers(Users users) {
+        return new CachedUsersImpl(users);
     }
 }
