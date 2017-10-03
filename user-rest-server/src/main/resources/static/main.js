0f7ff3b965d63f5066125c5143752737d04ce94a
P.Gavrilov <p.gavrilov@spb.ant-inform.ru>
2017-10-03 16:45:49 +0300

    web-interface

--- user-rest-server/src/main/resources/static/main.js
+++ user-rest-server/src/main/resources/static/main.js
@@ -9,8 +9,21 @@ $(function () {
             type: "GET",
             url: "/users",
             success: function (response) {
-                console.log(response)
+                console.log(new User(response[0]));
             }
         })
     });
 })
+
+function User(data) {
+    this.firstName = data.firstName;
+    this.middleName = data.middleName;
+    this.lastName = data.lastName;
+    this.birthDate = new Date(data.birthDate);
+    this.email = data.email;
+    this.login = data.login;
+    this.password = data.password;
+    this.loggedIn = data.loggedIn;
+    this.registerDate = new Date(data.registerDate);
+    this.updateDate = new Date(data.updateDate);
+}
